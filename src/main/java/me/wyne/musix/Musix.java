cf4a0956dcaf6339c8d45bfcbb0915ce456cf648
Wyne <izmodenov1997@gmail.com>
2024-09-20 19:24:36 +0500

    Configure project

--- src/main/java/me/wyne/musix/Musix.java
+++ src/main/java/me/wyne/musix/Musix.java
@@ -1,10 +1,16 @@
 package me.wyne.musix;
 
+import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.ModInitializer;
 
-public class Musix implements ModInitializer {
+public class Musix implements ModInitializer, ClientModInitializer {
 
     @Override
     public void onInitialize() {
+    }
+
+    @Override
+    public void onInitializeClient() {
+
     }
 }
